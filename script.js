ced486225da2aa156f6ef2cb8aba7685162e0f5e
jocimarjsc <jocimarjsc@yahoo.com.br>
2023-06-28 23:06:05 +0100

    add function draw in canvas

--- script.js
+++ script.js
@@ -1,52 +1,11 @@
-const paperWidth = 100
-const paperHeight = 70
-const paperArea = paperWidth * paperHeight
-const paperUsedPercentage = 100
-
-const cutWidth = 47
-const cutHeight = 33
-const cutArea = cutWidth * cutHeight
-const cutUsedPercentage = (cutArea * paperUsedPercentage) / paperArea
-
-const restultWidthWidth = Math.floor(paperWidth / cutWidth)
-const resultHeightHeight = Math.floor(paperHeight / cutHeight)
-const quantityWidthWidth = restultWidthWidth * resultHeightHeight
-
-const resultWidthHeight = Math.floor(paperWidth / cutHeight)
-const resultHeightWidht = Math.floor(paperHeight /  cutWidth)
-const quantityWidthHeight = resultWidthHeight * resultHeightWidht
-
-if(quantityWidthWidth > quantityWidthHeight) {
-    const usedArea = cutUsedPercentage * quantityWidthWidth
-    const unsedArea = paperUsedPercentage - usedArea
-    const result = {
-        width: restultWidthWidth,
-        height: resultHeightHeight,
-        usedArea: usedArea.toFixed(2),
-        unsedArea: unsedArea.toFixed(2),
-        quantity: quantityWidthWidth
-
-    }
-    console.log(result)
-}else {
-    const usedArea = cutUsedPercentage * quantityWidthHeight
-    const unsedArea = paperUsedPercentage - usedArea
-    const result = {
-        width: resultWidthHeight,
-        height: resultHeightWidht,
-        usedArea: usedArea.toFixed(2),
-        unsedArea: unsedArea.toFixed(2),
-        quantity: quantityWidthHeight
-
-    }
-    console.log(result)
-}
-
 const calculate = document.querySelector("#preview")
 const used = document.querySelector("#used")
 const printResult = document.querySelector("#result")
+const canvas = document.querySelector("#cutPaper")
 
-calculate.addEventListener("click", e => {
+calculate.addEventListener("click", runApp)
+
+function runApp(e) {
     e.preventDefault()
     const paper = {
         width: document.querySelector("#paper-width").value,
@@ -74,6 +33,7 @@ calculate.addEventListener("click", e => {
     const resultHeightWidht = Math.floor(paper.height /  cut.width)
     const quantityWidthHeight = resultWidthHeight * resultHeightWidht
 
+    
     if(quantityWidthWidth > quantityWidthHeight) {
         const usedArea = cut.percentage * quantityWidthWidth
         const unsedArea = paper.percentage - usedArea
@@ -82,9 +42,12 @@ calculate.addEventListener("click", e => {
             height: resultHeightHeight,
             usedArea: usedArea.toFixed(2),
             unsedArea: unsedArea.toFixed(2),
-            quantity: quantityWidthWidth
+            quantity: quantityWidthWidth,
+            horizontal: true
     
         }
+
+        initialCanvas(canvas, paper, cut, result)
         
         used.innerHTML = `<span>${result.usedArea}% utilizado</span><span>${result.unsedArea}% não utilizado</span>`
         printResult.innerHTML = `<span>Cabe <strong>${result.quantity}</strong> tamanhos <strong>${cut.width}x${cut.height}cm</strong>, em ${paper.width}x${paper.height}cm</span>`
@@ -96,15 +59,16 @@ calculate.addEventListener("click", e => {
             height: resultHeightWidht,
             usedArea: usedArea.toFixed(2),
             unsedArea: unsedArea.toFixed(2),
-            quantity: quantityWidthHeight
+            quantity: quantityWidthHeight,
+            horizontal: false
     
         }
         
+        initialCanvas(canvas, paper, cut, result)
         used.innerHTML = `<span>${result.usedArea}% utilizado</span><span>${result.unsedArea}% não utilizado</span>`
         printResult.innerHTML = `<span>Cabe <strong>${result.quantity}</strong> tamanhos <strong>${cut.width}x${cut.height}cm</strong>, em ${paper.width}x${paper.height}cm</span>`
     }
-    
-})
+}
 
 function calculateArea(width, height) {
     return width * height
@@ -112,4 +76,80 @@ function calculateArea(width, height) {
 
 function caculatePercentage (cutArea, paperPercentagem, paperArea) {
     return (cutArea * paperPercentagem) / paperArea
+}
+
+function initialCanvas(canvas, paper, cut, result) {
+    const ctx = canvas.getContext("2d")
+
+    const paperWidth = Math.max(paper.width, paper.height)
+    const paperHeight = Math.min(paper.width, paper.height)
+
+    const cutWidth = Math.max(cut.width, cut.height)
+    const cutHeight = Math.min(cut.width, cut.height)
+
+    const i = 333 / paperWidth
+    canvas.width = `${paperWidth * i}`
+    canvas.height = `${paperHeight * i}`
+
+    canvas.style.backgroundColor = "gray"
+    canvas.style.borderRadius = "0"
+    
+    ctx.fillStyle = "white"
+    ctx.strokeStyle = "black"
+    ctx.font = '12px serif'
+
+    draw(ctx, result.width, result.height, result.quantity, canvas.width,canvas.height, cutWidth,cutHeight, result.horizontal, i)
+
+    ctx.fillStyle = "black"
+    ctx.fillText(cut.height,(cut.height * i) / 2,(cut.width * i) - 1)
+    ctx.fillText(cut.width,(cut.height * i) /+ 1,(cut.width * i) / 2)
+
+}
+
+function draw(ctx,qtWidth, qtHeight, total, canvasWidth, canvasHeight, cutWidth, cutHeight, horizontal, i) {
+    let gridWidth = 0
+    let gridHeight = 0
+
+    if(horizontal) {
+        gridWidth = cutWidth * i
+        gridHeight = cutHeight * i
+    } else {
+        gridWidth = cutHeight * i
+        gridHeight = cutWidth * i
+    }
+
+
+    for (var x = 0; x <= canvasWidth; x += gridWidth) {
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, canvasHeight);
+    }
+
+    for (var y = 0; y <= canvasHeight; y += gridHeight) {
+        ctx.moveTo(0, y);
+        ctx.lineTo(canvasWidth, y);
+    }
+
+    ctx.strokeStyle = "lightgray";
+    ctx.lineWidth = 1;
+
+
+    ctx.stroke();
+
+
+
+
+
+
+
+
+    // for (let index = 0; index < qtWidth; index++) {
+    //     let c = canvasWidth/qtWidth
+
+    //     ctx.fillRect(0,0, canvasWidth/qtWidth, canvasHeight/qtHeight)
+    //     ctx.strokeRect(0,0, canvasWidth/qtWidth, canvasHeight/qtHeight)
+
+    //     ctx.fillRect(c,0, canvasWidth/qtWidth, canvasHeight/qtHeight)
+    //     ctx.strokeRect(c,0, canvasWidth/qtWidth, canvasHeight/qtHeight)
+        
+    // }
 }
